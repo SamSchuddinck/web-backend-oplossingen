92e8efd8940417b6d72a712c779afd437f8141a9
TheNumber4 <Sam.Schuddinck@hotmail.be>
2014-09-23 11:29:03 +0200

    Opdracht Comments

---
+++ opdracht-comments/Deel 1/index.php
@@ -0,0 +1,19 @@
+<?php
+
+	/*
+		Sam Schuddinck	
+		sam.schuddinck@student.kdg.be
+	*/
+?>
+
+<!doctype html>
+<html>
+	<head>
+		<title>Opdracht Comments: Deel 1</title>
+	</head>
+	<body>
+		<h1>Oplossing Comments: Deel 1</h1>
+		<?php echo 'Sam Schuddinck' ?>
+
+	</body>
+</html>
---
+++ opdracht-comments/Deel 2/index.php
@@ -0,0 +1,20 @@
+<?php
+
+	/*
+		Sam Schuddinck	
+		sam.schuddinck@student.kdg.be
+	*/
+
+echo '<!doctype html>
+<html>
+	<head>
+		<title>Opdracht Comments: Deel 2</title>
+	</head>
+	<body>
+		<h1>Oplossing Comments: Deel 2</h1>
+		Sam Schuddinck
+
+	</body>
+</html>';
+
+?>
