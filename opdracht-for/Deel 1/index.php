5e61e77512d4228728a945ba6d97897496b7a66d
TheNumber4 <Sam.Schuddinck@hotmail.be>
2014-10-06 22:47:09 +0200

    Opdracht for Deel 1 & 2

---
+++ opdracht-for/Deel 1/index.php
@@ -0,0 +1,40 @@
+<?php
+
+	$getallen		=	array();
+
+	for ( $getal = 0; $getal < 100; $getal++ )
+	{
+		$getallen[]	=	$getal;
+	}
+
+	$getallen=	implode( ', ', $getallen );
+
+	/*
+
+	*/
+
+	$getallen2	=	array();
+	for ( $getal = 0; $getal < 100; $getal++ )
+	{
+		if ( $getal % 3 == 0 && $getal > 40 && $getal < 80 )
+		{
+			$getallen2[]	=	$getal;
+		}
+	}
+
+	$getallen2	=	implode( ', ', $getallen2 );
+?>
+	
+
+<!doctype html>
+<html>
+    <head>
+    	<title>Deel1</title>
+    </head>
+    <body>
+		<p>Getallen1: <?= $getallenReeks ?></p>
+
+		<p>Getallen2: <?= $getallenReeks2 ?></p>
+
+    </body>
+</html>
