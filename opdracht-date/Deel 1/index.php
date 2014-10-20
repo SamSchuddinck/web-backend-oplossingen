a55767a8383abcf5ce519f819a7c0bdef183094c
Sam Schuddinck <Sam.Schuddinck@hotmail.be>
2014-10-20 18:24:30 +0200

    Opdracht Date Deel 1

---
+++ opdracht-date/Deel 1/index.php
@@ -0,0 +1,18 @@
+<?php
+
+	$timeStamp	=	mktime( 22, 35, 25, 01, 21, 1904 );
+
+	$date	=	strftime( '%d %B %Y, %H:%M:%S %p', $timestamp );
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+    	<title>Opdracht Date: deel1</title>
+    </head>
+    <body>
+		<p>De timestamp van 22u 35m 25sec 21 januari 1904 is <?= $timeStamp ?></p>
+		<p>De timestamp omgezet naar een datum: <?= $date ?></p>
+    </body>
+</html>
